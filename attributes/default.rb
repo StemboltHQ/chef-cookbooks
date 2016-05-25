b51faf72b7d842cdccc5f30952cf1770148da73c
Alex Blackie <alex@alexblackie.com>
2016-05-25 11:39:21 -0700

    Split Ruby install prefix into attribute
    Just in case it changes; it's used in a variety of places.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 # Ruby
 
 node.default[:ruby_version] = "2.3.1"
+node.default[:ruby_prefix] = "/usr/local/ruby"
--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -2,8 +2,8 @@ remote_file "/tmp/ruby-#{node[:ruby_version]}.tar.gz" do
   source "https://s3.amazonaws.com/stembolt-rubies/ruby-#{node[:ruby_version]}.tar.gz"
 end
 
-directory "/usr/local/ruby"
+directory node[:ruby_prefix]
 execute "install ruby" do
-  command "tar -zxf /tmp/ruby-#{node[:ruby_version]}.tar.gz -C /usr/local/ruby"
-  not_if { File.exist?("/usr/local/ruby/bin/ruby") }
+  command "tar -zxf /tmp/ruby-#{node[:ruby_version]}.tar.gz -C #{node[:ruby_prefix]}"
+  not_if { File.exist?("#{node[:ruby_prefix]}/bin/ruby") }
 end
