355753676c228c2739571ae954a594ad7fa04194
Alex Blackie <alex@alexblackie.com>
2016-05-25 11:43:29 -0700

    Modify PATH for custom Ruby
    Now all users have a Ruby

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -3,7 +3,21 @@ remote_file "/tmp/ruby-#{node[:ruby_version]}.tar.gz" do
 end
 
 directory node[:ruby_prefix]
+
 execute "install ruby" do
   command "tar -zxf /tmp/ruby-#{node[:ruby_version]}.tar.gz -C #{node[:ruby_prefix]}"
   not_if { File.exist?("#{node[:ruby_prefix]}/bin/ruby") }
+end
+
+bash "insert ruby into bashrc" do
+  # Since on Debian-based distros it aborts at the top if it's non-interactive,
+  # we can't just append a line, we need to hack it into the top. Thanks, Debian.
+  code <<-EOR
+ed /etc/bash.bashrc <<EOF
+0a
+export PATH=#{node[:ruby_prefix]}/bin:$PATH
+.
+wq
+EOF
+EOR
 end
